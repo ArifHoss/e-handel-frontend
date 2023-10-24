613ce78e56fed678ba8ff503210635afbf3462c0
Arif Hossain <hossaininfo@ymail.com>
2023-10-24 14:50:22 +0200

    Created Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from "./componets/Header.jsx";
 
 function App() {
 
   return (
     <>
         <h1>Hello world! This is Arif and Miwa!</h1>
+        <Header></Header>
+
     </>
   )
 }
---
+++ src/componets/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <h1>Hello from Header</h1>
+    );
+}
+
+export default Header;
