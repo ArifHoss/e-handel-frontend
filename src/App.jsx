9741059153e64868734a277684fa1ecf2fe2181a
Miwa G <miwa.guhres@hotmail.com>
2023-10-26 14:56:34 +0200

    Add Login Logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import React from 'react';
 import './App.css'
 import Header from "./componets/Header.jsx";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./componets/Navbar.jsx";
+
 
 function App() {
 
   return (
-    <>
-        <h1>Hello world! This is Arif and Miwa!</h1>
-        <Header></Header>
 
-    </>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element= {<Header/>}/>
+
+      </Routes>
+      
+       
+
+    </Router>
   )
 }
+import { formatPostcssSourceMap } from 'vite';
 
 export default App
---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Navbar= () =>{
+return (
+    <nav>
+      <Link to="/" >Home</Link>
+      <Link to="/header" >Header</Link>
+      <Link to="/login" >Login</Link>
+      <Link to="/logout" >Logout</Link>
+
+
+    </nav>
+)
+
+}
+export default Navbar;
