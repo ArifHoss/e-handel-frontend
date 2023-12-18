a4440aa8e3bc5d32b88fccc41a0fa412a3c6b630
Miwa G <miwa.guhres@hotmail.com>
2023-12-18 14:34:06 +0100

    Add sidebar

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,18 +1,28 @@
 // eslint-disable-next-line no-unused-vars
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
+import * as FaIcons from "react-icons/fa";
 import {Link} from "react-router-dom";
 import styles from "./Navbar.module.css";
 import {AuthContext} from "./AuthContext.jsx";
 import Logout from "./Logout.jsx";
+import {sidebarData} from "./NavbarSideData.jsx";
 
 const Navbar = () => {
     const { loggedIn, user } = useContext(AuthContext);
+    const [sidebar, setSidebar] =useState(false);
+    const showSidebar =() =>setSidebar(!sidebar); //If sidebar is true, it becomes false, and vice versa. it switches the value of sidebar between true and false.
+    console.log("sidebar",sidebar)
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.leftLinks}>
-            </div>
+    <>
+      <nav className={styles.navbar} >
+
             <div className={styles.title}>
-                <Link to="/">AM-GIGANTEN</Link>
+                <Link to="#" onClick={showSidebar}>
+                    <FaIcons.FaBars />
+                </Link>
+                <Link to="/">
+                    AM-GIGANTEN
+                </Link>
             <input type="text" placeholder="Sök efter produkt, kategori eller artikel" className={styles.searchInput} />
             </div>
 
@@ -35,8 +45,28 @@ const Navbar = () => {
                     }
                 </ul>
             </div>
-
         </nav>
+        <div className={sidebar? `${styles.sidebar} ${styles.active}`: `${styles.sidebar}`}>
+         <ul className={styles.sidebarItems} onClick={showSidebar}>
+          <li className={styles.sidebarToggle}>
+          <Link to="#">
+            <FaIcons.FaRegWindowClose onClick={showSidebar}/>
+          </Link>
+          </li>
+          {sidebarData.map((item , index)=>{
+            return(
+                <li key={index} className={item.cName}>
+                    <Link to={item.path} className={styles.sidebarText}>
+                        {item.icon}
+                        <span>{item.title}</span>
+                    </Link>
+                </li>
+            )
+          })}
+         </ul>
+        </div>
+        </>
+        
     )
 
 }
